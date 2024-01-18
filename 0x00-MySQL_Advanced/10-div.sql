4c41a87926de37ac91e7521e365538cd4529a35b
Oluwamarcellus <haydexwisdom@gmail.com>
2024-01-18 22:38:11 +0000

    Task 10 fixed

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,6 +1,7 @@
--- SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+-- SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER //
-CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS INTEGER
+CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT
 BEGIN
 	RETURN IF(b = 0, 0, a/b);
 END//
